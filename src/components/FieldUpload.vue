6b132636df003793781362b4a2fb43246294193b
Oleg Chervontsev <chervontsev@gmail.com>
2022-06-30 07:29:53 +0300

    Added file upload func

--- src/components/FieldUpload.vue
+++ src/components/FieldUpload.vue
@@ -1,24 +1,75 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import useStore from "@/store";
 
 export default defineComponent({
   setup() {
+    const { setData } = useStore();
     const fileInput = ref(null);
+    const fileName = ref("");
 
-    return { fileInput };
+    const handlePickFile = () => {
+      if (fileInput.value) {
+        fileName.value = "";
+        const el = fileInput.value as HTMLInputElement;
+        el.click();
+      }
+    };
+
+    const handleFilePicked = (e: Event) => {
+      const el = e.target as HTMLInputElement;
+      let files = el.files as FileList | null;
+
+      if (files && files[0]?.size) {
+        const reader = new FileReader();
+        reader.readAsText(files[0], "utf-8");
+        reader.onload = (e) => {
+          if (e.target?.result) {
+            const json = e.target.result as string;
+            const { data } = JSON.parse(json);
+            setData(data, () => {
+              fileName.value = files[0].name;
+            });
+          }
+        };
+      }
+    };
+
+    return { fileInput, fileName, handlePickFile, handleFilePicked };
   }
 })
 </script>
 
 <template>
   <div class="field-upload">
-    <input v-show="false" type="file" ref="fileInput" />
-    <button>
+    <input 
+      v-show="false" 
+      type="file" 
+      ref="fileInput" 
+      accept="application/JSON"
+      @change="handleFilePicked"
+    />
+    <button @click="handlePickFile">
       Upload
     </button>
+    <div class="file-name">
+      {{ fileName }}
+    </div>
   </div>
 </template>
 
 <style scoped>
-.field-upload {}
+.field-upload {
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.file-name {
+  flex: 1;
+  padding: 0 16px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,28 @@
 import { reactive, toRefs, Ref } from "vue";
 
+type Callback = () => void;
 type Data = Ref<number[]>;
 
 interface Store {
   state: { data: Data };
-  setData: (payload: number[]) => void;
+  setData: (payload: number[], fn?: Callback) => void;
 }
 const state = reactive({
   data: [1, 1, 1, 1, 1]
 });
 
 export default (): Store => {
-  const setData = (payload) => {
+  const validate = (payload: number[]): boolean => {
+    const match =
+      Array.isArray(payload) &&
+      payload.every((n: number) => typeof n === "number");
+    return match;
+  };
+  const setData = (payload: number[], callback?: Callback) => {
+    const match = validate(payload);
+    if (!match) return;
     state.data = payload;
+    callback && callback();
   };
 
   return {
