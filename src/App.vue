9bbeca13d3bcdd5da100460cd427948d3c8101ed
Oleg Chervontsev <chervontsev@gmail.com>
2022-06-30 03:37:38 +0300

    Added base CSS setup

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,4 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-figure {
-  margin: 0;
-}
 </style>
